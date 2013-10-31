457e25e0f7c8dbd74f2b956699546cad9a2b6c42
John Carr <john.carr@isotoma.com>
2013-10-31 22:41:06 +0000

    Can't pickle module objects

---
+++ bug_1603/celery.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+from celery import Celery
+from . import settings
+
+app = Celery("bug_1603.celery")
+app.config_from_object(settings)
+
+@app.task
+def add(x, y):
+    print "hello"
+
---
+++ bug_1603/settings.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+
+BROKER_URL = 'amqp://'
+CELERY_RESULT_BACKEND = 'cache+memcached://127.0.0.1:11211/'
+# CELERYD_POOL = 'gevent'
+
+from celery.schedules import crontab
+
+CELERYBEAT_SCHEDULE = {
+    'every-minute': {
+        'task': 'bug_1603.celery.add',
+        'schedule': crontab(minute='*/1'),
+        'args': (1,2),
+    }
+}
+
